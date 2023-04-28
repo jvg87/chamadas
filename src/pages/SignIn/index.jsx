88e2c5c1e74a217ef53138632eb3502f24d9d0fc
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-04-28 15:58:26 -0300

    iniciando o projeto

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,11 @@
+
+
+function SignIn() {
+    return ( 
+        <div>
+            <h1>Página Login</h1>
+        </div>
+    );
+}
+
+export default SignIn;
---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,11 @@
+
+
+function SignUp() {
+    return ( 
+        <div>
+            <h1>Página Cadastro</h1>
+        </div>
+    );
+}
+
+export default SignUp;
