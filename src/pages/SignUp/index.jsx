79d49e6d58c8a3f599fd3fdf6789ed4738443888
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-04-29 22:10:21 -0300

    funcionalidades do signup

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -7,13 +7,21 @@ function SignUp() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    function handleSubmit(e){
+        e.preventDefault()
+
+        if (name !== '' && email !== '' && password !== ''){
+            alert('fazer cadastro')
+        }
+    }
+
     return ( 
         <div className='container-center'>
             <div className='login'>
                 <div className='login-area'>
                     <img src={logo} alt="Logo do sistema de chamados" />
                 </div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h1>Nova Conta</h1>
                     <input 
                         type="text"
